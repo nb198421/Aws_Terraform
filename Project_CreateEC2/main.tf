4e52ebde58965617178868ecd01c278b962bc877
nb198421 <nb198421@gmail.com>
2023-08-12 17:12:47 +0100

    update code to create ec2

--- Project_CreateEC2/main.tf
+++ Project_CreateEC2/main.tf
@@ -1,1 +1,12 @@
-HI
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instance1" {
+  ami           = "ami-08a52ddb321b32a8c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terrafom_instance"
+  }
+}
